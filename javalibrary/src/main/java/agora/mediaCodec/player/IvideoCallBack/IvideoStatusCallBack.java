fda3ee10d80ee1c88080b14d3645239064ddafb2
TongJiangyong <1246376353@qq.com>
2018-10-23 00:23:44 +0800

    build adcTool

---
+++ javalibrary/src/main/java/agora/mediaCodec/player/IvideoCallBack/IvideoStatusCallBack.java
@@ -0,0 +1,10 @@
+package agora.mediaCodec.player.IvideoCallBack;
+
+/**
+ * Created by hasee on 2017/11/1.
+ */
+
+public interface IvideoStatusCallBack {
+    //获取视频的长度
+    void videoLong(float time);
+}
